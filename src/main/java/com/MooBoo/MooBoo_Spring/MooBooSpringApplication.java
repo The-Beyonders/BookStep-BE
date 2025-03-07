6ae22e1b5f9ac3ff8f2b2d8b7bdd7ebdc985f64d
yoo20370 <yoo20370@gmail.com>
2025-03-07 15:37:25 +0900

    [Add] dependency, p6spy library, Junit4, set .env

--- src/main/java/com/MooBoo/MooBoo_Spring/MooBooSpringApplication.java
+++ src/main/java/com/MooBoo/MooBoo_Spring/MooBooSpringApplication.java
@@ -2,6 +2,7 @@ package com.MooBoo.MooBoo_Spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MooBooSpringApplication {
@@ -9,5 +10,4 @@ public class MooBooSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MooBooSpringApplication.class, args);
 	}
-
 }
