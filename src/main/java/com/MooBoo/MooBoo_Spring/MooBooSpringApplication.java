162d8f1fb24864ca9a252647f0e44e76a0f9a8dd
yoo20370 <yoo20370@gmail.com>
2025-03-07 14:42:37 +0900

    init Repository

---
+++ src/main/java/com/MooBoo/MooBoo_Spring/MooBooSpringApplication.java
@@ -0,0 +1,13 @@
+package com.MooBoo.MooBoo_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooBooSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooBooSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MooBoo/MooBoo_Spring/MooBooSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MooBoo.MooBoo_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooBooSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
