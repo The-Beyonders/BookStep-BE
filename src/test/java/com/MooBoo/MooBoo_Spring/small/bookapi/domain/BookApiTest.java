f7a3e7f2a901bcac118be89a405471c7b9221d4a
yoo20370 <yoo20370@gmail.com>
2025-03-22 01:05:02 +0900

    [test/chore] BookApi 도메인의 toBookSearchResponse 메서드 테스트 및 테스트를 위한 application.yml 파일 자동 추가를 위해  CI/CD 수정

--- src/test/java/com/MooBoo/MooBoo_Spring/small/bookapi/domain/BookApiTest.java
+++ src/test/java/com/MooBoo/MooBoo_Spring/small/bookapi/domain/BookApiTest.java
@@ -1,8 +1,6 @@
 package com.MooBoo.MooBoo_Spring.small.bookapi.domain;
 
-import com.MooBoo.MooBoo_Spring.bookapi.domain.BookApi;
-import com.MooBoo.MooBoo_Spring.bookapi.domain.BookApiResponse;
-import com.MooBoo.MooBoo_Spring.bookapi.domain.BookItem;
+import com.MooBoo.MooBoo_Spring.bookapi.domain.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -38,11 +36,43 @@ public class BookApiTest {
         Assertions.assertThat(bookApi1_cover).isEqualTo("https://image.aladin.co.kr/product/48/91/coversum/8972833894_1.gif");
         Assertions.assertThat(bookApi1_publisher).isEqualTo("도우출판");
         Assertions.assertThat(bookApi1_pubDate).isEqualTo("2025-03-21");
-        Assertions.assertThat(bookApi1_itemPage).isEqualTo(533);
+        Assertions.assertThat(bookApi1_itemPage).isEqualTo("533");
+
+    }
+    
+    @Test
+    public void toBookSearchResponseTest() throws Exception {
+        //given
+        BookApi bookApi = BookApi.builder()
+                .isbn13("9788994492049")
+                .title("자바의 정석")
+                .description("자바에 대한 책입니다.")
+                .author("남궁성")
+                .cover("https://image.aladin.co.kr/product/48/91/coversum/8972833894_1.gif")
+                .publisher("도우출판")
+                .pubDate("2025-03-21")
+                .itemPage("533")
+                .build();
+        //when
+
+        BookSearchResponse bookSearchResponse = bookApi.toBookSearchResponse();
+
+        //then
+        Assertions.assertThat(bookSearchResponse.getIsbn13()).isEqualTo("9788994492049");
+        Assertions.assertThat(bookSearchResponse.getTitle()).isEqualTo("자바의 정석");
+        Assertions.assertThat(bookSearchResponse.getDescription()).isEqualTo("자바에 대한 책입니다.");
+        Assertions.assertThat(bookSearchResponse.getAuthor()).isEqualTo("남궁성");
+        Assertions.assertThat(bookSearchResponse.getCover()).isEqualTo("https://image.aladin.co.kr/product/48/91/coversum/8972833894_1.gif");
+        Assertions.assertThat(bookSearchResponse.getPublisher()).isEqualTo("도우출판");
+        Assertions.assertThat(bookSearchResponse.getPubDate()).isEqualTo("2025-03-21");
+        Assertions.assertThat(bookSearchResponse.getItemPage()).isEqualTo("533");
 
     }
 
     private static BookApiResponse getBookApiResponse() {
+        BookInfo bookInfo1 = new BookInfo();
+        bookInfo1.setItemPage("553");
+
         BookItem bookItem1 = BookItem.builder()
                 .isbn13("9788994492049")
                 .title("자바의 정석")
@@ -51,10 +81,14 @@ public class BookApiTest {
                 .cover("https://image.aladin.co.kr/product/48/91/coversum/8972833894_1.gif")
                 .publisher("도우출판")
                 .pubDate("2025-03-21")
-                .itemPage(533)
+                .bookInfo(bookInfo1)
                 .build();
 
+        BookInfo bookInfo2 = new BookInfo();
+        bookInfo2.setItemPage("553");
+
         BookItem bookItem2 = BookItem.builder()
+
                 .isbn13("9788994492049")
                 .title("자바의 정석")
                 .description("자바에 대한 책입니다.")
@@ -62,7 +96,7 @@ public class BookApiTest {
                 .cover("https://image.aladin.co.kr/product/48/91/coversum/8972833894_1.gif")
                 .publisher("도우출판")
                 .pubDate("2025-03-21")
-                .itemPage(533)
+                .bookInfo(bookInfo2)
                 .build();
 
         List<BookItem> bookItems = new ArrayList<>();
